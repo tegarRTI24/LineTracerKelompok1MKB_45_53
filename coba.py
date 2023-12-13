6331f18faa8fd66f8ce73d390c229c3d2cfb00fe
tegarRTI24 <90808656+tegarRTI24@users.noreply.github.com>
2023-12-13 09:33:28 +0700

    Update

--- coba.py
+++ coba.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6b324c9a0a39b1f1c501b3cf6df4e54610efdf6140550ee0edf15c02b3c30d97
-size 246272
+oid sha256:c50be7dcecc3fc11b9ce7d24c6780c9cd2c70209b5075045e4b4162a5ac25571
+size 246139
